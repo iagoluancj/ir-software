c9e1e53a9f19b9b07e811443183fd76752b9c608
Iago Luan <iago.luancj@gmail.com>
2025-05-26 09:58:50 -0300

    Ajustes de meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 const geistRoboto = Roboto({
   subsets: ["latin"],
   variable: "--font-roboto",
-  weight: ["100", "300", "400", "500", "700", "900"], 
+  weight: ["100", "300", "400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -30,6 +31,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>IR SOFTWARES | Desenvolvimento de Sistemas e Apps Sob Medida</title>
+        <meta name="description" content="Soluções personalizadas em desenvolvimento de sistemas e aplicativos para pequenas e médias empresas. Tire sua ideia do papel com a IR SOFTWARES." />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="IR SOFTWARES" />
+        <meta name="keywords" content="desenvolvimento de software, aplicativos, sistemas, IR SOFTWARES, sistemas sob medida, apps, desenvolvimento" />
+
+        {/* Open Graph para redes sociais */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="IR SOFTWARES | Desenvolvimento de Sistemas e Apps Sob Medida" />
+        <meta property="og:description" content="Soluções personalizadas em desenvolvimento de sistemas e aplicativos." />
+        <meta property="og:url" content="https://irsoftwares.com.br" />
+        <meta property="og:image" content="https://irsoftwares.com.br/caminho-da-imagem-de-preview.png" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="IR SOFTWARES | Desenvolvimento de Sistemas e Apps Sob Medida" />
+        <meta name="twitter:description" content="Soluções personalizadas em desenvolvimento de sistemas e aplicativos." />
+        <meta name="twitter:image" content="https://irsoftwares.com.br/caminho-da-imagem-de-preview.png" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${geistRoboto.variable} antialiased`}
       >
